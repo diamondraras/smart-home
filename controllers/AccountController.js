a6bf98143f91452ade50dd964ae3421af7f858a0
nyandrianina <nyandrianinamamy@gmail.com>
2018-09-03 13:40:15 +0300

    add ngrok, synchro controller

---
+++ controllers/AccountController.js
@@ -0,0 +1,12 @@
+const ngrok = require('ngrok');
+const AUTH_TOKEN = '2gC9bUJHcNZsjDvXWxYhM_WhP17b5Aq3p6WmtLrkYM'
+
+
+const synchronize = async function(req, res){
+    await ngrok.authtoken(AUTH_TOKEN);
+    const url = await ngrok.connect();
+
+    console.log(url);
+
+    res.send({url});
+}
