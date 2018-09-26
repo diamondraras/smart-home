32a22f87bb46a21bc401ffd568a14b5d137ad714
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2018-09-26 16:48:35 +0300

    Implements date filter

--- src/app/views/dashboard/history/history.component.ts
+++ src/app/views/dashboard/history/history.component.ts
@@ -6,7 +6,7 @@ import * as fromDashboard from '../dashboard.reducer';
 import { Entry } from '../../../shared/models/entry.model';
 import { Passing } from '../../../shared/models/passing.model';
 import { Store } from '@ngrx/store';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 import { removeSummaryDuplicates } from '@angular/compiler';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -17,17 +17,43 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class HistoryComponent implements OnInit {
   filter = 'entry';
+  newEntries;
   entries$: Observable<Entry[]>;
   passing$: Observable<Passing[]>;
 
-  constructor(private store: Store<fromDashboard.State>, private domSanitizer: DomSanitizer) { }
+  constructor(
+    private store: Store<fromDashboard.State>,
+    private domSanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
     this.entries$ = this.store.select(fromDashboard.getEntries);
     this.passing$ = this.store.select(fromDashboard.getPassing);
   }
 
-  show(b64string: string) {
-    console.log(this.domSanitizer.bypassSecurityTrustUrl(b64string));
+  sort(value: string) {
+    if (value !== null && value !== '') {
+      this.entries$ = this.store
+        .select(fromDashboard.getEntries)
+        .pipe(
+          map((entries: Entry[]) =>
+            entries.filter(
+              (entry: Entry) => new Date(entry.date) >= new Date(value)
+            )
+          )
+        );
+      this.passing$ = this.store
+        .select(fromDashboard.getPassing)
+        .pipe(
+          map((passings: Passing[]) =>
+            passings.filter(
+              (passing: Passing) => new Date(passing.date) >= new Date(value)
+            )
+          )
+        );
+    } else {
+      this.entries$ = this.store.select(fromDashboard.getEntries);
+      this.passing$ = this.store.select(fromDashboard.getPassing);
+    }
   }
 }
