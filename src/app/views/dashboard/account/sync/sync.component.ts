d7623ef5eff74433bf1a57854dbdb1b71b9fdf6d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2018-09-27 21:41:16 +0300

    Clean code

--- src/app/views/dashboard/account/sync/sync.component.ts
+++ src/app/views/dashboard/account/sync/sync.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../../shared/models/user.model';
 
 @Component({
   selector: 'app-sync',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sync.component.css']
 })
 export class SyncComponent implements OnInit {
+  currentUser: User;
 
   constructor() { }
 
   ngOnInit() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
 }
